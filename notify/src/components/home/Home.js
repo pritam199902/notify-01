8d003598c588dae571772357c332a35fabd40f99
Pritam Das <49154874+pritam199902@users.noreply.github.com>
2021-04-03 21:35:05 +0530

    home.js updated by pritam from master

--- notify/src/components/home/Home.js
+++ notify/src/components/home/Home.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
-            <h2>Testing</h2>
-            <h2>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</h2>
+            <div className="card">
+                <h1>Hello i am pritam</h1>
+
+            </div>
         </div>
     )
 }
